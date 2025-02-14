1e5ea99f9f181bb158f2b2ca251aa873c2898f4c
LOWE KUMAR <35005893+LoweKumar@users.noreply.github.com>
2025-02-14 12:35:08 +0000

    first attempt at using data to create a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
 st.title("Hello World")
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
